1d43d654884481321d25f43d058d78f26daaeac1
YoussefSaybi <147932529+YoussefSaybi@users.noreply.github.com>
2023-11-25 15:00:49 +0100

    Add files via upload

---
+++ gestionmagasino/vendeur.java
@@ -0,0 +1,11 @@
+package tn.tuniprod.gestionmagasino;
+
+public class vendeur extends employer{
+    public int TauxDeVente;
+
+    public vendeur (int TauxDeVente,int id,String nom, String adresse,double nbrheur){
+
+        super( id, nom, adresse, nbrheur);
+        this.TauxDeVente=TauxDeVente;
+    }
+}
